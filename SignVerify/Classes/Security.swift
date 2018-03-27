876e52c48274348acc821969eb3a09c2c5232812
Hassan Shahbazi <hassan@Vancosyss-MacBook-Pro.local>
2018-03-27 15:30:19 +0430

    Sign and Verify functions are added

--- SignVerify/Classes/Security.swift
+++ SignVerify/Classes/Security.swift
@@ -7,7 +7,105 @@
 //
 
 import UIKit
+import Security
 
 class Security: NSObject {
+    
+    private var keyAlgo:    CFString!
+    private var keySize:    NSNumber!
+    private var signAlgo:   SecKeyAlgorithm!
+    private var keyAccess:  CFString!
+    
+    init(KeyType:           CFString = kSecAttrKeyTypeEC,
+         KeySize:           Int = 256,
+         SignAlgorithm:     SecKeyAlgorithm = .ecdsaSignatureDigestX962SHA256,
+         KeycahinAccess:    CFString = kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly) {
+        
+        super.init()
+        keyAlgo     = KeyType
+        keySize     = NSNumber(value: KeySize)
+        signAlgo    = SignAlgorithm
+        keyAccess   = KeycahinAccess
+    }
+    
+    func generateKeyPair(PublicKeyID pubkey: String, PrivateKeyID pkey: String) -> (SecKey?, SecKey?) {
+        var publicKey:  SecKey?
+        var privateKey: SecKey?
+        
+        var privateKeyAttribute = [CFString:Any]()
+        var publicKeyAttribute  = [CFString:Any]()
+        var keyPairAttribute    = [CFString:Any]()
 
+        privateKeyAttribute[kSecAttrIsPermanent]    = NSNumber(value: true)
+        privateKeyAttribute[kSecAttrAccessible]     = keyAccess
+        privateKeyAttribute[kSecAttrApplicationTag] = pkey.data(using: .utf8)
+        
+        publicKeyAttribute[kSecAttrIsPermanent]     = NSNumber(value: true)
+        publicKeyAttribute[kSecAttrAccessible]      = keyAccess
+        publicKeyAttribute[kSecAttrApplicationTag]  = pubkey.data(using: .utf8)
+
+        keyPairAttribute[kSecAttrType]              = keyAlgo
+        keyPairAttribute[kSecAttrKeySizeInBits]     = keySize
+        keyPairAttribute[kSecPrivateKeyAttrs]       = privateKeyAttribute
+        keyPairAttribute[kSecPublicKeyAttrs]        = publicKeyAttribute
+        
+        SecKeyGeneratePair(keyPairAttribute as CFDictionary, &publicKey, &privateKey)
+        return (publicKey, privateKey)
+    }
+    
+    func getKey(ID id: String) -> SecKey? {
+        return queryKeychain(ID: id, Type: kSecReturnRef) as! SecKey?
+    }
+    
+    func getKey(ID id: String) -> Data? {
+        return queryKeychain(ID: id, Type: kSecReturnData) as? Data
+    }
+    
+    func checkSignVerifySupport(Key id: String) -> Bool {
+        if let key: SecKey = getKey(ID: id) {
+            return SecKeyIsAlgorithmSupported(key, .sign, signAlgo)
+        }
+        return false
+    }
+    
+    func sign(Data data: Data, PrivateKey pkey: String) throws -> Data?  {
+        var error: Unmanaged<CFError>?
+        if let key: SecKey = getKey(ID: pkey) {
+            if let signature = SecKeyCreateSignature(key, signAlgo, data as CFData, &error) {
+                return signature as Data?
+            }
+            throw error!.takeRetainedValue() as Error
+        }
+        return nil
+    }
+
+    func verify(RawData raw: Data, SignedData data: Data, PublicKey pubkey: String) throws -> Bool {
+        var error: Unmanaged<CFError>?
+        if let key: SecKey = getKey(ID: pubkey) {
+            if SecKeyVerifySignature(key, signAlgo, raw as CFData, data as CFData, &error) {
+                return true
+            }
+            throw error!.takeRetainedValue() as Error
+        }
+        return false
+    }
+
+
+}
+
+
+extension Security {
+    private func queryKeychain(ID id: String, Type type: CFString) -> AnyObject? {
+        var keyAttribute  = [CFString:Any]()
+        var key:  AnyObject?
+        
+        keyAttribute[kSecAttrType]            = keyAlgo
+        keyAttribute[kSecClass]               = kSecClassKey
+        keyAttribute[kSecAttrApplicationTag]  = id.data(using: .utf8)
+        keyAttribute[type]                    = NSNumber(value: true)
+        keyAttribute[kSecAttrAccessible]      = keyAccess
+        
+        SecItemCopyMatching(keyAttribute as CFDictionary, &key)
+        return key
+    }
 }
--- SignVerifyTests/SignVerifyTests.swift
+++ SignVerifyTests/SignVerifyTests.swift
@@ -7,30 +7,82 @@
 //
 
 import XCTest
+import Security
 @testable import SignVerify
 
 class SignVerifyTests: XCTestCase {
     
+    let rawData = "t5$A1ZIPdPbyi*)n-9vDJKPfoYW%Ld35VIJ8QBgn$rCUtiRhlqWdx2Pe^O-qL%R&".data(using: .utf8)!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_1_keyGeneration() {
+        let security = Security()
+        let (pubKey, pKey) = security.generateKeyPair(PublicKeyID: "PublicKeyID", PrivateKeyID: "PrivateKeyID")
+        
+        XCTAssertNotNil(pubKey)
+        XCTAssertNotNil(pKey)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_2_keyData() {
+        let security = Security()
+        let pubkey: Data? = security.getKey(ID: "PublicKeyID")
+        let pkey: Data? = security.getKey(ID: "PrivateKeyID")
+        
+        XCTAssertNotNil(pubkey)
+        XCTAssertNotNil(pkey)
+    }
+    
+    func test_3_keyRef() {
+        let security = Security()
+        let pubkey: SecKey? = security.getKey(ID: "PublicKeyID")
+        let pkey: SecKey? = security.getKey(ID: "PrivateKeyID")
+        
+        XCTAssertNotNil(pubkey)
+        XCTAssertNotNil(pkey)
+    }
+    
+    func test_4_signSupport() {
+        let security = Security(SignAlgorithm: .ecdsaSignatureDigestX962SHA256)
+        XCTAssertTrue(security.checkSignVerifySupport(Key: "PrivateKeyID"))
+        
+        let security2 = Security(SignAlgorithm: .ecdhKeyExchangeCofactorX963SHA1)
+        XCTAssertFalse(security2.checkSignVerifySupport(Key: "PrivateKeyID"))
+        
+        let security3 = Security(SignAlgorithm: .ecdsaSignatureDigestX962SHA1)
+        XCTAssertTrue(security3.checkSignVerifySupport(Key: "PrivateKeyID"))
+    }
+    
+    func test_5_signData() {
+        let security = Security(SignAlgorithm: .ecdsaSignatureDigestX962SHA512)
+        do {
+            let sign = try security.sign(Data: rawData, PrivateKey: "PrivateKeyID")
+            XCTAssertNotNil(sign)
+        }
+        catch let error {
+            print(error)
+            XCTAssertFalse(true)
         }
     }
-    
+
+    func test_6_verifySign() {
+        let security = Security(SignAlgorithm: .ecdsaSignatureDigestX962SHA512)
+        do {
+            let sign = try security.sign(Data: rawData, PrivateKey: "PrivateKeyID")
+            XCTAssertNotNil(sign)
+            
+            let verify = try security.verify(RawData: rawData, SignedData: sign!, PublicKey: "PublicKeyID")
+            XCTAssertTrue(verify)
+        }
+        catch let error {
+            print(error)
+            XCTAssertFalse(true)
+        }
+    }
 }
