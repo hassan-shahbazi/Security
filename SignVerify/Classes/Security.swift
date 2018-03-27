dae9c6012f140668a74c427783b0e6b88ce26bd8
Hassan Shahbazi <hassan@Vancosyss-MacBook-Pro.local>
2018-03-27 11:35:55 +0430

    Initial commit

---
+++ SignVerify/Classes/Security.swift
@@ -0,0 +1,13 @@
+//
+//  Security.swift
+//  SignVerify
+//
+//  Created by Hassan Shahbazi on 2018-03-27.
+//  Copyright © 2018 Hassan Shahbazi. All rights reserved.
+//
+
+import UIKit
+
+class Security: NSObject {
+
+}
